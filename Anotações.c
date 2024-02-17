8fdaff834f712a78967cd299efda9cfac9e8b1dc
Lucas Santos <115794858+Lucas714174@users.noreply.github.com>
2024-02-17 12:22:52 -0300

    Update Anotações.c

--- Anotações.c
+++ Anotações.c
@@ -1,6 +1,33 @@
+//**********************************************HELLO_WORLD*****************************************************************
+
 #include <stdio.h>
 
 int main() {
   printf("Hello World!");
   return 0;
 }
+
+//*************************************************USER_INPUT*****************************************************************
+
+#include <stdio.h>
+
+int main() {
+  // Cria variáveis int e char
+  int myNum;
+  char myChar;
+
+  // Pede que o user insira um N° e um caracter
+  printf("Type a number AND a character and press enter: \n");
+
+  // Armazena o end. de memória da variável
+  scanf("%d %c", &myNum, &myChar);
+
+  // Printa o numero
+  printf("Your number is: %d\n", myNum);
+
+  // Printa o caracter
+  printf("Your character is: %c\n", myChar);
+  
+  return 0;
+}
+

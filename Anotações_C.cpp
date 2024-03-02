9626a09c5b2ad30de2062b7c454aec4d223c9b0f
Lucas Santos <115794858+Lucas714174@users.noreply.github.com>
2024-03-01 21:01:19 -0300

    Create Anotações_C.cpp

---
+++ Anotações_C.cpp
@@ -0,0 +1,9 @@
+//**************************************************SINTAXE_C++***********************************************
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  cout << "I am learning C++";
+  return 0;
+}

46160c87e572b17aa3519e94e037a72946d456ea
Lucas Santos <115794858+Lucas714174@users.noreply.github.com>
2024-03-01 22:39:37 -0300

    Update Anotações_C.cpp

--- Anotações_C.cpp
+++ Anotações_C.cpp
@@ -1,9 +1,27 @@
 //**************************************************SINTAXE_C++***********************************************
+
 #include <iostream>
 using namespace std;
 
 int main() {
   cout << "Hello World!" << endl;
   cout << "I am learning C++";
+  return 0;
+}
+
+//**********************************************CIN>>*******************************************************
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum;
   return 0;
 }
